b92b0fba763585e8db281828174b0418c2acb528
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2025-05-08 13:54:12 +0200

    upload classes

---
+++ lab6/Faktury1/src/rabaty/ObliczCenePoRabacieProcentowym.java
@@ -0,0 +1,9 @@
+package rabaty;
+
+public class ObliczCenePoRabacieProcentowym implements ObliczCenePoRabacie{
+
+    @Override
+    public double obliczCenePoRabacie(double cena) {
+        return cena * 0.9;
+    }
+}
